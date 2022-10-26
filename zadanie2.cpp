2e2e3b7a0784398d25d2b0c0b6677ebf2d2fa960
kgul1 <113103268+kgul1@users.noreply.github.com>
2022-10-26 16:11:10 +0200

    Add files via upload

---
+++ zadanie2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+main(void)
+{
+    float a, b, wynik;
+    cout << "podaj liczbe a: ";
+    cin >> a;
+    cout << "podaj liczbe b: ";
+    cin >> b;
+    if
+
+        (a > b)
+
+        cout << "liczba a jest wieksza od liczby b";
+
+    else
+        cout << "liczba b jest wieksza od liczby a";
+    return 0;
+}
